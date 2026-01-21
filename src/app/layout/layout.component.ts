cd8dce4a1dfb08cb8a25851c6be14e93bdfc5340
Pablo Quirino <pabloquirino17@gmail.com>
2026-01-21 11:12:38 -0300

    feat(meus-anuncios): add HTML for meus-anuncios route

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterOutlet } from "@angular/router";
+import { RouterOutlet } from "@angular/router";
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
+  standalone: true,
   imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
--- src/app/layout/meus-anuncios/meus-anuncios.component.ts
+++ src/app/layout/meus-anuncios/meus-anuncios.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-meus-anuncios',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './meus-anuncios.component.html',
   styleUrl: './meus-anuncios.component.scss'
 })
 export class MeusAnunciosComponent {
+  myAds = [
+    {
+      brand: 'Toyota',
+      model: 'Corolla',
+      year: 2020,
+      km: 45000,
+      price: 75000,
+      image: '/assets/cars/corolla.jpg'
+    }
+  ];
 
 }

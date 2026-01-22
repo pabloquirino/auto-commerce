9d7314a3372084138498d5ab0e975b16e0f32b3e
Pablo Quirino <pabloquirino17@gmail.com>
2026-01-21 21:37:21 -0300

    style: customize select and option styles

--- src/app/layout/explorar/explorar.component.ts
+++ src/app/layout/explorar/explorar.component.ts
@@ -15,27 +15,61 @@ import { Car } from '../../models/car.model';
 })
 export class ExplorarComponent implements OnInit {
 
-  searchTerm = '';
-  isFilterOpen = false;
-
   cars: Car[] = [];
   filteredCars: Car[] = [];
+  brands: string[] = [];
+  years: number[] = [];
+  selectedBrand: string = '';
+  searchTerm = '';
+  selectedYear: number | '' = '';
+  selectedPriceRange = '';
+
+  isFilterOpen = false;
 
   constructor(private carService: CarService) { }
 
   ngOnInit() {
     this.cars = this.carService.getAll();
     this.filteredCars = this.cars;
+
+    // anos
+    this.years = [...new Set(this.cars.map(car => car.year))]
+      .sort((a, b) => b - a);
+
+    // marcas
+    this.brands = [...new Set(this.cars.map(car => car.brand))]
+      .sort();
   }
 
-  onSearch() {
+  applyFilters() {
     const term = this.searchTerm.toLowerCase();
 
-    this.filteredCars = this.cars.filter(car =>
-      car.brand.toLowerCase().includes(term) ||
-      car.model.toLowerCase().includes(term) ||
-      car.year.toString().includes(term)
-    );
+    this.filteredCars = this.cars.filter(car => {
+      const matchesSearch =
+        car.brand.toLowerCase().includes(term) ||
+        car.model.toLowerCase().includes(term) ||
+        car.year.toString().includes(term);
+
+      const matchesYear =
+        !this.selectedYear || car.year === this.selectedYear;
+
+      const matchesBrand =
+        !this.selectedBrand || car.brand === this.selectedBrand;
+
+      const matchesPrice = (() => {
+        if (!this.selectedPriceRange) return true;
+
+        if (this.selectedPriceRange === '200000+') {
+          return car.price >= 200000;
+        }
+
+        const [min, max] = this.selectedPriceRange.split('-').map(Number);
+        return car.price >= min && car.price <= max;
+      })();
+
+      return matchesSearch && matchesYear && matchesBrand && matchesPrice;
+    });
   }
+
 
 }
